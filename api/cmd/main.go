e2447eec8158f2b0cc3eb4cf1425e6627b99511b
Perry Claunch <pclaunch@firstorion.com>
2023-07-24 15:00:27 -0500

    convert to gin router

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
-
-	"price-check-api/models"
-	"price-check-api/utils"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -18,9 +14,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
-	chiproxy "github.com/awslabs/aws-lambda-go-api-proxy/chi"
-	"github.com/go-chi/chi/v5"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
 )
 
 type CurrentChallenge struct {
@@ -38,7 +33,7 @@ type Result struct {
     Suggestion string `json:"imageUrls"`
 }
 
-var adapter *chiproxy.ChiLambda
+var ginLambda *ginadapter.GinLambda
 var svc *dynamodb.Client
 
 func init() {
@@ -52,20 +47,20 @@ func init() {
 
 	svc = dynamodb.NewFromConfig(cfg)
 	
-    r := chi.NewRouter()
+    r := gin.Default()
 
-	r.Post("/challenge", HandlePostItem)
+	// r.POST("/challenge", HandlePostItem)
 	// r.Get("/challenge", HandleListItems)
 	// r.Put("/challenge/{id}", HandlePutItem)
-	r.Get("/challenge/{id}", HandleGetItem)
-    r.Post("/challenge/{id}/guess", HandleChallengeGuess)
-	r.Get("/challenge/today", HandleCurrentChallenges)
+	// r.GET("/challenge/{id}", HandleGetItem)
+    // r.POST("/challenge/{id}/guess", HandleChallengeGuess)
+	r.GET("/challenge/today", HandleCurrentChallenges)
 
-    adapter = chiproxy.New(r)
+    ginLambda = ginadapter.New(r)
 }
 
 func lambdaHandler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    c, err := adapter.ProxyWithContext(ctx, req)
+    c, err := ginLambda.ProxyWithContext(ctx, req)
 
     return c, err
 }
@@ -74,7 +69,7 @@ func main() {
     lambda.Start(lambdaHandler)
 }
 
-func HandleCurrentChallenges(w http.ResponseWriter, r *http.Request) {
+func HandleCurrentChallenges(c *gin.Context) {
     tableName := "challenges"
     today := time.Now().Format("2006-01-02")
 
@@ -82,10 +77,10 @@ func HandleCurrentChallenges(w http.ResponseWriter, r *http.Request) {
 	expr, err := expression.NewBuilder().WithKeyCondition(keyExpr).Build()
     if err != nil {
         log.Fatalf(fmt.Sprintf("Failed to build expression, %v", err))
-        http.Error(w, http.StatusText(500), 500)
+        c.JSON(http.StatusInternalServerError, err)
     }
     
-    result, err := svc.Query(r.Context(), &dynamodb.QueryInput{
+    result, err := svc.Query(c, &dynamodb.QueryInput{
         TableName: aws.String(tableName),
 		IndexName: aws.String("date-id-index"),
 		ExpressionAttributeNames:  expr.Names(),
@@ -94,12 +89,12 @@ func HandleCurrentChallenges(w http.ResponseWriter, r *http.Request) {
     })
     if err != nil {
         log.Fatalf("Got error calling GetItem: %s", err)
-        http.Error(w, http.StatusText(404), 404)
+        c.JSON(http.StatusNotFound, err)
     }
 
     if result.Items == nil{
         log.Fatalf("Could not find challenge for today")
-        http.Error(w, http.StatusText(404), 404)
+        c.JSON(http.StatusNotFound, err)
     }
 
 	type Response struct {
@@ -112,121 +107,121 @@ func HandleCurrentChallenges(w http.ResponseWriter, r *http.Request) {
     err = attributevalue.UnmarshalListOfMaps(result.Items, &items)
     if err != nil {
         log.Fatalf(fmt.Sprintf("Failed to unmarshal Record, %v", err))
-        http.Error(w, http.StatusText(500), 500)
+        c.JSON(http.StatusInternalServerError, err)
     }
-
-	utils.RespondWithJSON(w, 200, items)
+	
+	c.JSON(http.StatusOK, items)
 }
 
-func HandleChallengeGuess(w http.ResponseWriter, r *http.Request) {
-    tableName := "challenges"
-	id := chi.URLParam(r, "id")
+// func HandleChallengeGuess(c *gin.Context) {
+//     tableName := "challenges"
+// 	id := chi.URLParam(r, "id")
 
-    var reqBody *Submission
-    err := json.NewDecoder(r.Body).Decode(&reqBody)
-    if err != nil {
-        log.Fatalf("Unmarshalling error: %s", err)
-        http.Error(w, http.StatusText(500), 500)
-    }
+//     var reqBody *Submission
+//     err := json.NewDecoder(r.Body).Decode(&reqBody)
+//     if err != nil {
+//         log.Fatalf("Unmarshalling error: %s", err)
+//         http.Error(w, http.StatusText(500), 500)
+//     }
     
-    challenge, err := svc.GetItem(r.Context(), &dynamodb.GetItemInput{
-        TableName: aws.String(tableName),
-		Key: map[string]types.AttributeValue{
-			"id": &types.AttributeValueMemberS{Value: id},
-		},
-    })
-    if err != nil {
-        log.Fatalf("Got error calling GetItem: %s", err)
-        http.Error(w, http.StatusText(500), 500)
-    }
+//     challenge, err := svc.GetItem(r.Context(), &dynamodb.GetItemInput{
+//         TableName: aws.String(tableName),
+// 		Key: map[string]types.AttributeValue{
+// 			"id": &types.AttributeValueMemberS{Value: id},
+// 		},
+//     })
+//     if err != nil {
+//         log.Fatalf("Got error calling GetItem: %s", err)
+//         http.Error(w, http.StatusText(500), 500)
+//     }
 
-    if challenge.Item == nil{
-        log.Fatalf("Could not find challenge for today")
-        http.Error(w, http.StatusText(400), 400)
-    }
+//     if challenge.Item == nil{
+//         log.Fatalf("Could not find challenge for today")
+//         http.Error(w, http.StatusText(400), 400)
+//     }
     
-    var item *models.Item
+//     var item *models.Item
     
-    err = attributevalue.UnmarshalMap(challenge.Item, &item)
-    if err != nil {
-        log.Fatalf(fmt.Sprintf("Failed to unmarshal Record, %v", err))
-        http.Error(w, http.StatusText(500), 500)
-    }
+//     err = attributevalue.UnmarshalMap(challenge.Item, &item)
+//     if err != nil {
+//         log.Fatalf(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+//         http.Error(w, http.StatusText(500), 500)
+//     }
 
-    result := Result {
-        Result: "Incorrect",
-        Suggestion: "Go Lower",
-    }
+//     result := Result {
+//         Result: "Incorrect",
+//         Suggestion: "Go Lower",
+//     }
 
-    if reqBody.PriceGuess == item.Price {
-        result = Result{
-            Result: "Correct",
-            Suggestion: "Nice Job!",
-        }
-		utils.RespondWithJSON(w, 200, result)
-    }
+//     if reqBody.PriceGuess == item.Price {
+//         result = Result{
+//             Result: "Correct",
+//             Suggestion: "Nice Job!",
+//         }
+// 		utils.RespondWithJSON(w, 200, result)
+//     }
 
-    if reqBody.PriceGuess < item.Price {
-        result = Result{
-            Result: "Incorrect",
-            Suggestion: "Go Higher",
-        }
-		utils.RespondWithJSON(w, 200, result)
-    }
+//     if reqBody.PriceGuess < item.Price {
+//         result = Result{
+//             Result: "Incorrect",
+//             Suggestion: "Go Higher",
+//         }
+// 		utils.RespondWithJSON(w, 200, result)
+//     }
 
-	utils.RespondWithJSON(w, 200, result)
-}
+// 	utils.RespondWithJSON(w, 200, result)
+// }
 
-func HandlePostItem(w http.ResponseWriter, r *http.Request) {
-    tableName := "challenges"
+// func HandlePostItem(c *gin.Context) {
+//     tableName := "challenges"
 
-	var item *models.Item
-    err := json.NewDecoder(r.Body).Decode(&item)
-    if err != nil {
-        log.Fatalf("Unmarshalling error: %s", err)
-        http.Error(w, http.StatusText(500), 500)
-    }
+// 	var item *models.Item
+//     err := json.NewDecoder(r.Body).Decode(&item)
+//     if err != nil {
+//         log.Fatalf("Unmarshalling error: %s", err)
+//         http.Error(w, http.StatusText(500), 500)
+//     }
 
-	av, err := attributevalue.MarshalMap(item)
-    if err != nil {
-        log.Fatalf("Error marshalling to attributevalues: %s", err)
-        http.Error(w, http.StatusText(500), 500)
-    }
+// 	av, err := attributevalue.MarshalMap(item)
+//     if err != nil {
+//         log.Fatalf("Error marshalling to attributevalues: %s", err)
+//         http.Error(w, http.StatusText(500), 500)
+//     }
     
-    result, err := svc.PutItem(r.Context(), &dynamodb.PutItemInput{
-        TableName: aws.String(tableName),
-        Item: av,
-    })
-    if err != nil {
-        log.Fatalf("Got error calling PutItem: %s", err)
-        http.Error(w, http.StatusText(500), 500)
-    }
+//     result, err := svc.PutItem(r.Context(), &dynamodb.PutItemInput{
+//         TableName: aws.String(tableName),
+//         Item: av,
+//     })
+//     if err != nil {
+//         log.Fatalf("Got error calling PutItem: %s", err)
+//         http.Error(w, http.StatusText(500), 500)
+//     }
 
-	utils.RespondWithJSON(w, 200, result)
-}
+// 	utils.RespondWithJSON(w, 200, result)
+// }
 
-func HandleGetItem(w http.ResponseWriter, r *http.Request) {
-    tableName := "challenges"
-	id := chi.URLParam(r, "id")
+// func HandleGetItem(c *gin.Context) {
+//     tableName := "challenges"
+// 	id := chi.URLParam(r, "id")
     
-    var challenge *models.Item
+//     var challenge *models.Item
     
-    res, err := svc.GetItem(r.Context(), &dynamodb.GetItemInput{
-        TableName: aws.String(tableName),
-		Key: map[string]types.AttributeValue{
-			"id": &types.AttributeValueMemberS{Value: id},
-		},
-    })
-    if err != nil {
-        log.Fatalf("Got error calling GetItem: %s", err)
-        http.Error(w, http.StatusText(500), 500)
-    }
+//     res, err := svc.GetItem(r.Context(), &dynamodb.GetItemInput{
+//         TableName: aws.String(tableName),
+// 		Key: map[string]types.AttributeValue{
+// 			"id": &types.AttributeValueMemberS{Value: id},
+// 		},
+//     })
+//     if err != nil {
+//         log.Fatalf("Got error calling GetItem: %s", err)
+//         http.Error(w, http.StatusText(500), 500)
+//     }
     
-    err = attributevalue.UnmarshalMap(res.Item, &challenge)
-    if err != nil {
-        log.Fatalf(fmt.Sprintf("Failed to unmarshal Record, %v", err))
-        http.Error(w, http.StatusText(500), 500)
-    }
+//     err = attributevalue.UnmarshalMap(res.Item, &challenge)
+//     if err != nil {
+//         log.Fatalf(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+//         http.Error(w, http.StatusText(500), 500)
+//     }
 
-	utils.RespondWithJSON(w, 200, challenge)
-}
+// 	utils.RespondWithJSON(w, 200, challenge)
+// }
