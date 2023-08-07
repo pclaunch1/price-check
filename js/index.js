3fc1dc716bce9aa2ab167714ef4e3c077faf01c9
Perry Claunch <pclaunch@firstorion.com>
2023-08-07 16:53:49 -0500

    adding pull for purchase url

--- js/index.js
+++ js/index.js
@@ -21,21 +21,25 @@ function submitGuess(e) {
     e.preventDefault();
     const priceGuess = document.getElementById("guessValue").value
     
-    const truncated = Math.trunc(priceGuess)
-    
-    var result = "WRONG - LOWER"
-    if(truncated === config.PRODUCT_PRICE){
-        result = "CORRECT!!!"
-        const buyBtn = document.getElementById("buyProductBtn")
-        buyBtn.setAttribute('href', config.PRODUCT_URL)
-        buyBtn.style.display = "block"
-    }
-    
-    if(truncated < config.PRODUCT_PRICE){
-        result = "WRONG - HIGHER"
-    }
-
-    document.getElementById("feedback").innerText = result
+    postData("https://jvhttn9e7f.execute-api.us-east-1.amazonaws.com/test/challenge/"+ productId + "/guess", { priceGuess: parseInt(priceGuess), attemptCount: 1 }).then((data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
+        document.getElementById("feedback").innerText = data.result + " -- " + data.suggestion
+        if(data.result === "Correct"){
+          fetch("https://jvhttn9e7f.execute-api.us-east-1.amazonaws.com/test/challenge/" + productId)
+          .then((response) => {
+              if (response.ok) {
+                  return response.json();
+              } else {
+                  throw new Error("API CALL FAIL");
+              }
+          })
+          .then(data => {
+            const buyBtn = document.getElementById("buyProductBtn")
+            buyBtn.setAttribute('href', data.purchaseUrl)
+            buyBtn.style.display = "block"
+          });
+        }
+      });
 };
 
 function displayChallenge(data) {
@@ -45,3 +49,18 @@ function displayChallenge(data) {
 
     imageDiv.src = challengeImage;
 }
+
+async function postData(url = "", data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify(data),
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
