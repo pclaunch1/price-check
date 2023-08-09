0b7040e3ae15900f8c75c965448b564e6379ace7
Perry Claunch <pclaunch@firstorion.com>
2023-08-09 17:07:07 -0500

    cleaning javascript

--- js/index.js
+++ js/index.js
@@ -1,64 +1,85 @@
 let productId;
-
 let slideIndex = 1;
+let apiHost = "https://jvhttn9e7f.execute-api.us-east-1.amazonaws.com/test";
 
-fetch("https://jvhttn9e7f.execute-api.us-east-1.amazonaws.com/test/challenge/today")
-.then((response) => {
-    if (response.ok) {
-        return response.json();
-    } else {
-        throw new Error("API CALL FAIL");
-    }
-})
-.then(data => {
-    productId = data[0]["id"];
-    displayChallenge(data[0]);
-    showSlides(slideIndex);
+getTodayChallenge();
+
+var form = document.getElementById("guessForm");
+form.addEventListener('submit', submitGuess);
+
+$("input[data-type='currency']").on({
+  keyup: function() {
+    formatCurrency($(this));
+  },
+  blur: function() { 
+    formatCurrency($(this), "blur");
+  }
 });
 
-var form = document.getElementById("guessForm")
-
-form.addEventListener('submit', submitGuess)
+function getTodayChallenge(){
+  fetch(apiHost + "/challenge/today")
+  .then((response) => {
+      if (response.ok) {
+          return response.json();
+      } else {
+          throw new Error("API CALL FAIL");
+      }
+  })
+  .then(data => {
+      productId = data[0]["id"];
+      displayChallenge(data[0]);
+      showSlides(slideIndex);
+  });
+}
 
 function submitGuess(e) {
     e.preventDefault();
-    const priceGuess = document.getElementById("guessValue").value
-    
-    postData("https://jvhttn9e7f.execute-api.us-east-1.amazonaws.com/test/challenge/"+ productId + "/guess", { priceGuess: parseInt(priceGuess), attemptCount: 1 }).then((data) => {
-        // document.getElementById("feedback").innerText = data.result + " -- " + data.suggestion
-        let guessList = document.getElementById("guessList")
-        let guessDiv = document.createElement("div")
-        let suggestionDiv = document.createElement("span")
-        if(data.suggestion === "Go Higher"){
-          suggestionDiv.setAttribute("class", "fa fa-chevron-up")
-        }
-        else if(data.suggestion === "Go Lower"){
-          suggestionDiv.setAttribute("class", "fa fa-chevron-down")
-        }
-        else{
-          suggestionDiv.setAttribute("class", "fa fa-check")
-        }
-        guessDiv.innerText = priceGuess
+    const priceGuess = document.getElementById("guessValue").value;
+    const reqBody = { priceGuess: parseInt(priceGuess), attemptCount: 1 };
 
-        guessList.appendChild(guessDiv)
-        guessList.appendChild(suggestionDiv)
-        if(data.result === "Correct"){
-          fetch("https://jvhttn9e7f.execute-api.us-east-1.amazonaws.com/test/challenge/" + productId)
-          .then((response) => {
-              if (response.ok) {
-                  return response.json();
-              } else {
-                  throw new Error("API CALL FAIL");
-              }
-          })
-          .then(data => {
-            const buyBtn = document.getElementById("buyProductBtn")
-            buyBtn.setAttribute('href', data.purchaseUrl)
-            buyBtn.style.display = "block"
-          });
-        }
+    postData(apiHost + "/challenge/"+ productId + "/guess", reqBody).then((data) => {
+      appendGuess(data=data);
+      if(data.result === "Correct"){
+        displayBuyLink();
+      }
       });
 };
+
+function appendGuess(data = {}){
+  let guessList = document.getElementById("guessList");
+  let guessDiv = document.createElement("div");
+  let suggestionDiv = document.createElement("span");
+
+  if(data.suggestion === "Go Higher"){
+    suggestionDiv.setAttribute("class", "fa fa-chevron-up");
+  }
+  else if(data.suggestion === "Go Lower"){
+    suggestionDiv.setAttribute("class", "fa fa-chevron-down");
+  }
+  else{
+    suggestionDiv.setAttribute("class", "fa fa-check");
+  }
+  guessDiv.innerText = priceGuess;
+
+  guessList.appendChild(guessDiv);
+  guessList.appendChild(suggestionDiv);
+}
+
+function displayBuyLink(){
+  fetch(apiHost + "/challenge/" + productId)
+  .then((response) => {
+      if (response.ok) {
+          return response.json();
+      } else {
+          throw new Error("API CALL FAIL");
+      }
+  })
+  .then(data => {
+    const buyBtn = document.getElementById("buyProductBtn")
+    buyBtn.setAttribute('href', data.purchaseUrl)
+    buyBtn.style.display = "block"
+  });
+}
 
 function displayChallenge(data) {
     data.imageUrls.forEach((element, index) => {
@@ -124,80 +145,70 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-$("input[data-type='currency']").on({
-  keyup: function() {
-    formatCurrency($(this));
-  },
-  blur: function() { 
-    formatCurrency($(this), "blur");
-  }
-});
-
-
 function formatNumber(n) {
-// format number 1000000 to 1,234,567
-return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+  // format number 1000000 to 1,234,567
+  return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 }
 
 
 function formatCurrency(input, blur) {
-// appends $ to value, validates decimal side
-// and puts cursor back in right position.
+  // appends $ to value, validates decimal side
+  // and puts cursor back in right position.
 
-// get input value
-var input_val = input.val();
+  // get input value
+  var input_val = input.val();
 
-// don't validate empty input
-if (input_val === "") { return; }
+  // don't validate empty input
+  if (input_val === "") { return; }
 
-// original length
-var original_len = input_val.length;
+  // original length
+  var original_len = input_val.length;
 
-// initial caret position 
-var caret_pos = input.prop("selectionStart");
-  
-// check for decimal
-if (input_val.indexOf(".") >= 0) {
+  // initial caret position 
+  var caret_pos = input.prop("selectionStart");
+    
+  // check for decimal
+  if (input_val.indexOf(".") >= 0) {
 
-  // get position of first decimal
-  // this prevents multiple decimals from
-  // being entered
-  var decimal_pos = input_val.indexOf(".");
+    // get position of first decimal
+    // this prevents multiple decimals from
+    // being entered
+    var decimal_pos = input_val.indexOf(".");
 
-  // split number by decimal point
-  var left_side = input_val.substring(0, decimal_pos);
-  var right_side = input_val.substring(decimal_pos);
+    // split number by decimal point
+    var left_side = input_val.substring(0, decimal_pos);
+    var right_side = input_val.substring(decimal_pos);
 
-  // add commas to left side of number
-  left_side = formatNumber(left_side);
+    // add commas to left side of number
+    left_side = formatNumber(left_side);
 
-  // validate right side
-  right_side = formatNumber(right_side);
-  
-  // On blur make sure 2 numbers after decimal
-  if (blur === "blur") {
-    right_side += "00";
+    // validate right side
+    right_side = formatNumber(right_side);
+    
+    // On blur make sure 2 numbers after decimal
+    if (blur === "blur") {
+      right_side += "00";
+    }
+    
+    // Limit decimal to only 2 digits
+    right_side = right_side.substring(0, 2);
+
+    // join number by .
+    input_val = "$" + left_side;
+
+  } else {
+    // no decimal entered
+    // add commas to number
+    // remove all non-digits
+    input_val = formatNumber(input_val);
+    input_val = "$" + input_val;
   }
-  
-  // Limit decimal to only 2 digits
-  right_side = right_side.substring(0, 2);
 
-  // join number by .
-  input_val = "$" + left_side;
+  // send updated string to input
+  input.val(input_val);
 
-} else {
-  // no decimal entered
-  // add commas to number
-  // remove all non-digits
-  input_val = formatNumber(input_val);
-  input_val = "$" + input_val;
-}
-
-// send updated string to input
-input.val(input_val);
-
-// put caret back in the right position
-var updated_len = input_val.length;
-caret_pos = updated_len - original_len + caret_pos;
-input[0].setSelectionRange(caret_pos, caret_pos);
+  // put caret back in the right position
+  var updated_len = input_val.length;
+  caret_pos = updated_len - original_len + caret_pos;
+  input[0].setSelectionRange(caret_pos, caret_pos);
 }
